130e209bd21ac604ce2768033f31227f7110c8d4
ginovh <gino@ubuntu.com>
2017-05-05 13:13:16 +0200

    update localhost

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f^vfc!iazh2qs)-@ks2(x8g#vb#(gu03z(b4@k*#+n@u5dmv^z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
